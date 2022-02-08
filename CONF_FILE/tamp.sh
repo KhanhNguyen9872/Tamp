1bc6ec9ccca1adafe8f890206549d651214e9966
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2022-02-08 21:54:42 +0700

    Add files via upload

---
+++ CONF_FILE/tamp.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+option="${1}" 
+case ${option} in 
+   -start) 
+	   if ps -C httpd >/dev/null; then
+		   echo "apache2 service still running..."
+	   else
+	   	echo "Starting MySQL/Apache2...."
+		httpd &> /dev/null
+		mysqld --skip-grant-tables --general-log &> /dev/null
+		printf "   \b\b\b\b\b"
+		printf "\e[1;34m[\e[1;32m Done \e[1;34m]\e[0m";
+		echo "";
+	   fi
+      ;; 
+   -stop)
+   	   echo "Stopping MySQL/Apache2...."
+	   killall httpd
+	   killall mysqld &> /dev/null
+	   killall /data/data/com.termux/files/usr/bin/mariadbd &> /dev/null
+	   killall mysqld_safe &> /dev/null
+	   printf "   \b\b\b\b\b"
+	   printf "\e[1;34m[\e[1;32m Done \e[1;34m]\e[0m";
+	   echo "";
+
+      ;;
+   *)  
+      echo "KhanhNguyen9872"
+      echo "Github: https://github.com/khanhnguyen9872"
+      echo "FB: https://fb.me/khanh10a1"
+      exit 1
+      ;; 
+esac
